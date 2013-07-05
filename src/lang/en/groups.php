437bcdca88ee1e528a277c14b0cc583d8ee8c862
petercoles <peter@kuroiweb.com>
2013-07-05 14:59:46 +0100

    Minor typo

--- src/lang/en/groups.php
+++ src/lang/en/groups.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-    'no_group'       => 'No Group found.',
+    'no_group'       => 'No Groups found.',
     'create_success' => 'New group created.',
     'delete_success' => 'Group deleted.',
     'update_success' => 'Group updated.',
