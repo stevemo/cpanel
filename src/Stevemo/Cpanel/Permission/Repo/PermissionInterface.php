ff39509117a4431ce42dd190589bc7136d7124cf
Steve Montambeault <stevemo2003@gmail.com>
2013-11-05 13:14:52 +0800

    created PermissionInterface

---
+++ src/Stevemo/Cpanel/Permission/Repo/PermissionInterface.php
@@ -0,0 +1,53 @@
+<?php namespace Stevemo\Cpanel\Permission\Repo;
+
+interface PermissionInterface {
+
+    /**
+     * Grab all the permissions from storage
+     *
+     * @author Steve Montambeault
+     * @link   http://stevemo.ca
+     *
+     * @param array $columns
+     *
+     * @return \StdClass
+     */
+    public function all($columns = array('*'));
+
+    /**
+     * Put into storage a new permission
+     *
+     * @author Steve Montambeault
+     * @link   http://stevemo.ca
+     *
+     * @param array $data
+     *
+     * @return bool
+     */
+    public function create(array $data);
+
+    /**
+     * Update a permission into storage
+     *
+     * @author Steve Montambeault
+     * @link   http://stevemo.ca
+     *
+     * @param array $data
+     *
+     * @return bool
+     */
+    public function update(array $data);
+
+    /**
+     * Delete a permission from storage
+     *
+     * @author Steve Montambeault
+     * @link   http://stevemo.ca
+     *
+     * @param $id
+     *
+     * @return void
+     */
+    public function delete($id);
+
+} 
