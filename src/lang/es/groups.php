468aa0b9b7f210f48c329398ee63cb5a23b2f871
BNIETO <bnieto@80soluciones.com>
2013-11-11 21:28:59 +0100

    added Spanish translation @80Soluciones

---
+++ src/lang/es/groups.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'no_group'       => 'No se han encontrado grupos.',
+    'create_success' => 'Nuevo grupo creado.',
+    'delete_success' => 'Grupo borrado.',
+    'update_success' => 'Grupo modificado.',
+);  
