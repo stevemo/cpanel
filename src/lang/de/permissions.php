5d386aea65fadfffda949ecdb3e3c932a52e57c9
Steve Montambeault <stevemo2003@gmail.com>
2013-11-20 11:11:16 +0800

    added German lang file from Ludwig Mair

---
+++ src/lang/de/permissions.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+   'no_found'        => 'Keine Rechte für diese Gruppe gefunden.',
+   'create_success'  => 'Rechte erfolgreich erstellt.',
+   'model_not_found' => 'Diese Rechte existieren nicht.',
+   'update_success'  => 'Rechte verändert.',
+   'delete_success'  => 'Modul Rechte gelöst.',
+   'no_permission'   => 'Keine Rechte gefunden.',
+   'access_denied'   => 'Sie besitzen keine Rechte für diese Seite.',
+);
---
+++ src/lang/de/users.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'logout'                     => 'Logout war erfolgreich.',
+    'register_success'           => 'Gratulation! Die Registrierung war erfolgreich.',
+    'create_success'             => 'Benutzer angelegt.',
+    'update_success'             => 'Benutzer bearbeitet.',
+    'login_success'              => 'Sie sind erfolgreich angemeldet.',
+    'delete_denied'              => 'Achtung: Sie können sich nicht selber löschen.',
+    'delete_success'             => 'Benutzer gelöscht.',
+    'permissions_update_success' => 'Benutzer Rechte bearbeitet.',
+    'activation_success'         => 'Benutzer erfolgreich aktiviert.',
+    'activation_fail'            => 'Benutzer Aktivierung misslungen.',
+    'deactivation_success'       => 'Benutzer erfolgreich aktiviert.',
+);
