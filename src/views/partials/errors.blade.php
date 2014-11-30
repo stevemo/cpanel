f38d4dbe09d67a44d98d299ffbd2cde4c0506608
Steve Montambeault <stevemo2003@gmail.com>
2014-11-30 13:17:51 +0800

    view to display validation errors

---
+++ src/views/partials/errors.blade.php
@@ -0,0 +1,13 @@
+@if ($errors->any())
+    <div class="alert alert-danger alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert">
+            <span aria-hidden="true">&times;</span>
+            <span class="sr-only">Close</span>
+        </button>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
