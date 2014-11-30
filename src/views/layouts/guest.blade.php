a6d715cf7515b157ae5346b8c2062468fc67dcb7
Steve Montambeault <stevemo2003@gmail.com>
2014-11-30 13:17:22 +0800

    Layout for registration and login

---
+++ src/views/layouts/guest.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>AAG Philippines | Portal</title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <link href="/packages/stevemo/cpanel/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="/packages/stevemo/cpanel/vendor/fontawesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+
+    <link href="/packages/stevemo/cpanel/AdminLTE/css/guest.css" rel="stylesheet" type="text/css" />
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body class="skin-blue">
+
+    @yield('content')
+
+
+    <!-- jQuery 2.0.2 -->
+    <script src="/packages/stevemo/cpanel/vendor/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="/packages/stevemo/cpanel/vendor/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+
+</body>
+</html>
